a7c20cf91aa0f2c691ac9c695f7f271f9ce3286b
nhcan <nhcan@DC-N133>
2020-09-15 16:27:28 +0700

    Init Commit

---
+++ src/Abp/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace Abp.Extensions
+{
+    /// <summary>
+    /// Extension methods for String class.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// indicates whether this string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        [ContractAnnotation("null => true")]
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
---
+++ web/OffShoreAspNetBoilerplate.Web/Startup/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OffShoreAspNetBoilerplate.Web.Core.Configuration;
+
+namespace OffShoreAspNetBoilerplate.Web.Startup
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public Startup(/*IWebHostEnvironment env*/IConfiguration configuration)
+        {
+            Configuration = configuration;
+            //_appConfiguration = env.GetAppConfiguration();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
