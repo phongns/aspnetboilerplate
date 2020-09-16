38ac144720437ea926ab2ffcefb5c8c1a9fe49d2
nhcan <nhcan@DC-N133>
2020-09-16 11:14:09 +0700

    test commit other user

--- web/OffShoreAspNetBoilerplate.Web/Startup/Startup.cs
+++ web/OffShoreAspNetBoilerplate.Web/Startup/Startup.cs
@@ -35,8 +35,6 @@ namespace OffShoreAspNetBoilerplate.Web.Startup
                 .AddNewtonsoftJson();
 
             AuthConfigurer.Configure(services, _appConfiguration);
-
-            //services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
